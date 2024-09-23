2be729fc85b16c5dcd4eb64739d92d9bd4f8493f
b0x3n <b0x3n4dm1n@gmail.com>
2024-09-23 09:22:31 +0100

    Fix

--- public/js/bootstrap.js
+++ public/js/bootstrap.js
@@ -9,8 +9,8 @@
 
     const Main = () => {
 
-        window.__url = '';
-        // window.__url = 'https://b0x3n.github.io/keyline';
+        // window.__url = '';
+        window.__url = 'https://b0x3n.github.io/keylink';
 
         const   __app = App();
 
