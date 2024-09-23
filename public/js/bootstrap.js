13145d996b49197ea9f51e919d0d89076d432eac
b0x3n <b0x3n4dm1n@gmail.com>
2024-09-23 10:54:24 +0100

    Adding client images...

--- public/js/bootstrap.js
+++ public/js/bootstrap.js
@@ -9,8 +9,8 @@
 
     const Main = () => {
 
-        window.__url = '';
-        // window.__url = 'https://b0x3n.github.io/keylink';
+        // window.__url = '';
+        window.__url = 'https://b0x3n.github.io/keylink';
 
         const   __app = App();
 
